02141b922257125ff8c86324e6b60be01f8bd3fe
Matt Edwards <mttdwrds@gmail.com>
2013-06-20 17:37:45 -0400

    Added example data, filtering BED files, and the license.  Improved count histogram code.

--- make_dup_histo_from_bam.py
+++ make_dup_histo_from_bam.py
@@ -1,6 +1,19 @@
-#!/usr/bin/python2.6
-import pysam, sys, numpy, bisect
+#!/usr/bin/env python
+#
+# Census read duplicate histogram generator.
+#
+# See usage instructions in the README or http://github.com/matted/census/.
+#
+# Matt Edwards
+# Copyright 2013 MIT
+# Released under the MIT license
+#
+#
+
+import pysam, sys, numpy, bisect, re, argparse
 from collections import defaultdict
+
+VERSION = "0.8"
 
 def covered(regions, pos):
     targ = bisect.bisect_left(regions, (pos,0))
@@ -8,9 +21,9 @@ def covered(regions, pos):
         targ -= 1
     return regions[targ][0] <= pos and regions[targ][1] >= pos or regions[targ-1][0] <= pos and regions[targ-1][1] >= pos
 
-def parse_bed(fname):
+def parse_bed(fin_bed):
     bed = {}
-    fin_bed = open(fname)
+    # fin_bed = open(fname)
     total = 0
     sizes = []
     D = 0
@@ -18,8 +31,6 @@ def parse_bed(fname):
     for line in fin_bed:
         line = line.strip().split()
         chromo = line[0]
-        if len(chromo) > 5 or chromo.startswith("chrM") or chromo.startswith("chrX") or chromo.startswith("chrY"):
-            continue
         if not bed.has_key(chromo):
             bed[chromo] = [(0,0)]
         bed[chromo].append((int(line[1]) - D, int(line[2]) + D))
@@ -30,41 +41,50 @@ def parse_bed(fname):
         bed[chromo] = sorted(regions)
 
     print >>sys.stderr, "total bases excluded:", total
-    print >>sys.stderr, "chromosomes total:", len(bed)
-    if len(sizes) > 0:
-        print >>sys.stderr, "minimum size block:", min(sizes), numpy.average(sizes), numpy.median(sizes), numpy.std(sizes)
+    print >>sys.stderr, "total chromosomes included:", len(bed)
     
     fin_bed.close()
     return bed
 
-# if covered(bed[chromo1], pos1) or covered(bed[chromo2], pos2): continue
-
-def parse(s):
-    ret = s.split("/")[0]
-    ret = ret.split("#")[0]
-    # ret = ret.split(" ")[1] # for SRA reads.
-    # ret = s
-    # ret = ret.split(":")[1:5]
-    # ret = ret.split(":")[3:7] # for DNAse-seq reads, unsure of source
-    ret = ret.split(":")[-4:] # for DNAse-seq reads, unsure of source
-    # ret[-1] = ret[-1].split("_")[0].split("#")[0] # should regexp it...
-
-    return tuple(map(int, ret))
+def parse(s, regexp):
+    match = re.match(regexp, s)
+    if match is None:
+        # print >>sys.stderr, "warning: read name regular expression couldn't parse the flowcell position"
+        return BAD_COORDS
+    else:
+        return tuple(map(int, match.groups())) 
 
 def flowcell_dist(x,y):
-    if x[0] != y[0] or x[1] != y[1]:
+    if x[0] != y[0] or x[1] != y[1] or x[0] == -1 or y[0] == -1:
         return float("inf")
     return ((x[2]-y[2])**2.0 + (x[3]-y[3])**2.0)**0.5
 
-print >>sys.stderr, "trying to open [%s]" % sys.argv[2]
-samf = pysam.Samfile(sys.argv[2], 'rb')
-bed = parse_bed(sys.argv[1])
+parser = argparse.ArgumentParser(description="Histogram generator for Census library complexity package.")
+parser.add_argument("excluded_regions.bed", type=argparse.FileType('r'))
+parser.add_argument("sorted_reads.bam", type=argparse.FileType('rb'))
+parser.add_argument("-v", "--version", action="version", version=VERSION)
+
+parser.add_argument("-s", "--single_ended", action="store_true", dest="unpaired", help="Include only single-ended reads, instead of only paired-end reads where both ends map.")
+parser.add_argument("-q", "--mapq", type=int, default=1, help="Minimum read mapping quality for a read or read pair to be included.  Default is 1.")
+parser.add_argument("-d", "--mindist", type=int, default=100, help="Maximum distance in reported flowcell coordinates for reads to be considered optical duplicates.  Default is 100.")
+parser.add_argument("-r", "--regexp", default="[\w\. ]+:([\d]):([\d]+):([\d]+):([\d]+).*", help="Regular expression for finding flowcell coordinates from read names.  Default is [\w\. ]+:([\d]):([\d]+):([\d]+):([\d]+).*")
+
+args = parser.parse_args()
+
+samf = pysam.Samfile(getattr(args, "sorted_reads.bam").name, 'rb')
+bed = parse_bed(getattr(args, "excluded_regions.bed"))
 
 hits = {}
-records = 0
+records = 0.01
+valid = 0
 
 DEBUG = True
 
+MIN_DIST = args.mindist
+
+PAIRED = not args.unpaired
+
+MIN_MAPQ = args.mapq
 
 maxX = -1
 maxY = -1
@@ -75,28 +95,39 @@ badChrKilled = 0
 badMapqKilled = 0
 cnvsKilled = 0
 current = (-1,-1)
+BAD_COORDS = (-1,-1,-1,-1)
+parseFailure = False
 
 for read in samf:
-    # print dir(read)
     if read.is_secondary or read.is_unmapped or read.is_read2 or read.mate_is_unmapped:
         continue
-    if read.rname == read.mrnm and read.pos == read.mpos or read.mrnm == -1:
+    if read.rname == read.mrnm and read.pos == read.mpos:
         # print "skipping as mirrored read:", read.qname, read.is_paired, read.is_proper_pair, read.mate_is_unmapped, read.isize
         continue
-    # if records > 15000000:
-    #    break
 
-    if records % 1000000 == 0:
+    if records > 0 and records % 1000000 == 0:
         if DEBUG: print >>sys.stderr, "processed %d reads, max x: %d, max y: %d" % (records, maxX, maxY)
 
     records += 1
 
-    if read.mapq <= 0:
+    if PAIRED:
+        if read.mrnm == -1 or not read.is_read1:
+            continue
+    else:
+        if read.is_read1 or read.is_read2:
+            continue
+        read.mrnm = read.rname # hack to treat it as the paired case for optical dup detection, etc.
+        read.mpos = read.pos # same
+
+    valid += 1
+
+    if read.mapq < MIN_MAPQ:
         badMapqKilled += 1
         continue
        
     refname = samf.getrname(read.rname)
     mateRefname = samf.getrname(read.mrnm) 
+
     if len(bed) > 0 and (not bed.has_key(refname) or not bed.has_key(mateRefname)):
         badChrKilled += 1
         continue
@@ -106,10 +137,8 @@ for read in samf:
         continue
 
     if (samf.getrname(read.rname), read.pos) != current:
-        ### process hits
         for key, names in hits.iteritems():
-            if len(names) > 1:
-                # print key, names
+            if len(names) > 1 and BAD_COORDS not in names: # if we couldn't parse flowcell coordinates, don't try any optical duplicate detection
                 legit = set(names)
                 if len(legit) > 500:
                     if DEBUG: print >>sys.stderr, "skipping a cluster because it has too many hits"
@@ -118,32 +147,32 @@ for read in samf:
                 for index1, name1 in enumerate(names):
                     for index2 in xrange(index1+1, len(names)):
                         dist = flowcell_dist(name1, names[index2])
-                        # print "\t", name1, names[index2], dist
-                        if dist < 100:
+                        if dist < MIN_DIST:
                             legit.discard(names[index2])
-                # print "\t", len(legit)
                 if False and len(legit) >= 4:
                     print key, len(legit), sorted(legit)
                 histo[len(legit)] += 1
                 dupsKilled += len(names) - len(legit)
             else:
                 histo[len(names)] += 1
-        ### end processing
+
         hits.clear()
         current = (samf.getrname(read.rname), read.pos)
 
     key = (samf.getrname(read.rname), read.pos, samf.getrname(read.mrnm) if read.mrnm != -1 else "", read.mpos, read.isize)
+
     if not hits.has_key(key):
         hits[key] = []
-    coords = parse(read.qname)
+    coords = parse(read.qname, args.regexp)
+    if coords == BAD_COORDS:
+        parseFailure = True
     hits[key].append(coords)
     if coords[-2] > maxX: maxX = coords[-2]
     if coords[-1] > maxY: maxY = coords[-1]
     
-### process hits: code duplication!
+# process hits for the last set (duplicate code...)
 for key, names in hits.iteritems():
-    if len(names) > 1:
-        # print key, names
+    if len(names) > 1 and BAD_COORDS not in names:
         legit = set(names)
         if len(legit) > 500:
             if DEBUG: print >>sys.stderr, "skipping a cluster because it has too many hits"
@@ -152,35 +181,21 @@ for key, names in hits.iteritems():
         for index1, name1 in enumerate(names):
             for index2 in xrange(index1+1, len(names)):
                 dist = flowcell_dist(name1, names[index2])
-                # print "\t", name1, names[index2], dist
-                if dist < 200:
+                if dist < MIN_DIST:
                     legit.discard(names[index2])
-        # print "\t", len(legit)
         if False and len(legit) >= 4:
             print key, len(legit), sorted(legit)
         histo[len(legit)] += 1
         dupsKilled += len(names) - len(legit)
     else:
         histo[len(names)] += 1
-### end processing
       
 samf.close()
 
-# print len(hits)
-#histo = defaultdict(int)
-#for key in hits.iterkeys():
-#    histo[len(hits[key])] += 1
+if parseFailure:
+    print >>sys.stderr, "WARNING: read name regular expression couldn't parse the flowcell position, so no optical duplicate detection was performed"
 
-print sys.argv[2]
+print "# %d considered read pairs, %d valid read pairs, %d optical dup read pairs (%.2f pct.), %d bad chr read pairs (%.2f pct.), %d CNV read pairs (%.2f pct.), %d bad mapq pairs (%.2f pct.)" % (records, valid, dupsKilled, 100.0*dupsKilled/records, badChrKilled, 100.0*badChrKilled/records, cnvsKilled, 100.0*cnvsKilled/records, badMapqKilled, 100.0*badMapqKilled/records)
 
-print "after: ", sorted(histo.iteritems())
-print "removed %d (%.2f%%) optical duplicate read pairs" % (dupsKilled, 100.0*dupsKilled/records)
-print "removed %d (%.2f%%) bad chr read pairs" % (badChrKilled, 100.0*badChrKilled/records)
-print "removed %d (%.2f%%) CNV read pairs" % (cnvsKilled, 100.0*cnvsKilled/records)
-print "removed %d (%.2f%%) bad mapq read pairs" % (badMapqKilled, 100.0*badMapqKilled/records)
-
-fout = open("%s.popCNV_filt_histo.txt" % sys.argv[2], 'w')
-print >>fout, "#\t%d considered read pairs\t%d valid read pairs\t%d optical dup read pairs\t%.2f\tpct. optical dups\t%d bad chr read pairs\t%.2f\tpct. bad chrs\t%d CNV read pairs\t%.2f\tpct. CNVs" % (records, records-dupsKilled-badChrKilled-cnvsKilled, dupsKilled, 100.0*dupsKilled/records, badChrKilled, 100.0*badChrKilled/records, cnvsKilled, 100.0*cnvsKilled/records)
 for hit, count in sorted(histo.iteritems()):
-    print >>fout, "%d\t%d" % (hit, count)
-fout.close()
+    print "%d\t%d" % (hit, count)
