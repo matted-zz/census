2e8c5a8e94809ba69c10cde04a23725e37539b0c
Matt Edwards <mttdwrds@gmail.com>
2015-01-16 12:22:25 -0500

    Added setup.py to simplify installation and manage dependencies.

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+setup(name="census",
+      version="0.9",
+      description="Sequencing library complexity estimation.",
+      author="Matt Edwards",
+      author_email="matted@mit.edu",
+      license="MIT",
+      url="https://github.com/matted/census",
+      packages=[],
+      scripts=["calculate_libsize.py", "bam_to_histo.py"],
+      zip_safe=True,
+      install_requires=["scipy", "numpy", "pysam"],
+     )
